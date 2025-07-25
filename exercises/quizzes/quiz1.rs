3553c9151a7a10a4e62e8a575b50021d2a986c3f
liuanhao <liuanhao@cscec3b.com>
2025-07-25 18:39:29 +0800

    progress: primitive types4

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -16,6 +16,15 @@ fn main() {
     // You can optionally experiment here.
 }
 
+fn calculate_price_of_apples(num: i32) -> i32 {
+
+    if num <= 40 {
+        num * 2
+    } else {
+        num
+    }
+}
+
 // Don't change the tests!
 #[cfg(test)]
 mod tests {
